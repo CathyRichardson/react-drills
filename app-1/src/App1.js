4132e4e958982a82dfde99811e44898ba880c38c
Cathy Richardson <cathyanne08@gmail.com>
2021-06-17 19:22:22 -0600

    completed app-2

--- app-1/src/App1.js
+++ app-1/src/App1.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class App1 extends Component {
     constructor() {
-        super()
+        super();
 
         this.state = {
             input: '',
@@ -13,10 +13,17 @@ class App1 extends Component {
         this.setState({ input: event.target.value})
     }
 
+    // Another way to write it, see arrow function on commented out input below:
+    // handleChange(value) {
+    //     this.setState({ message: value });
+    //   }
+
     render() {
         return (
             <div>
                 <input onChange={this.handleInput} />
+                {/* <input onChange={e => this.handleChange(e.target.value)} type="text" /> */}
+                {/* <p>{this.state.message}</p> */}
                 <h1>{this.state.input}</h1>
             </div>
         )
