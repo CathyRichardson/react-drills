d2b47cb9ce9b0b98711e8103224fb59a3e8023ac
Cathy Richardson <cathyanne08@gmail.com>
2021-06-17 20:19:13 -0600

    completed app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      list: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"],
+      input: '',
+    }
+  }
+
+  handleInput = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  render() {
+    let list = this.state.list;
+    let input = this.state.input
+    let filteredList = list.filter(e => e.includes(input)) //note: includes returns true for an empty input string.
+
+    return (
+      <div className="App">
+        <input onChange={this.handleInput} />
+        <ul>
+          {filteredList.map((element, i) => <li key={i}>{element}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
